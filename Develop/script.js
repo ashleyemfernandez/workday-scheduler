dab263a3b04f6fdacfef4dc26f8edde148acc902
Ashley Em <ashleyemfernandez@outlook.com>
2023-05-18 21:48:55 -0400

    revised js

--- Develop/script.js
+++ Develop/script.js
@@ -1,38 +1,53 @@
-var saveButton = document.querySelector("saveBtn");
-var currentHour = dayjs().format('hh-mm-ss');
+$(document).ready(function () {
+ 
+  var currentTime = dayjs();
+  $("#currentDay").text(currentTime);
+
+  $("#hour9 .description").val(localStorage.getItem("hour9"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour1 .description").val(localStorage.getItem("hour1"));
+  $("#hour2 .description").val(localStorage.getItem("hour2"));
+  $("#hour3 .description").val(localStorage.getItem("hour3"));
+  $("#hour4 .description").val(localStorage.getItem("hour4"));
+  $("#hour5 .description").val(localStorage.getItem("hour5"));
 
 
-var today = dayjs().format; 
-$('#currentDay').text(today.format('MMM D, YYYY'));
+  function hourTracker() {
+      
+      var currentHour = moment().hour();
 
-function time() {
-  $(".time-block").each(function() {
-    var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+     
+      $(".time-block").each(function () {
+          var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+          console.log( blockHour, currentHour)
 
-    if (blockTime < currentHour) {
-      $(this).removeClass("future");
-      $(this).removeClass("present");
-      $(this).addClass("past");
-    } else if (blockTime === currentHour) {
-      $(this).removeClass("past");
-      $(this).addClass("present");
-      $(this).removeClass("future");
-    } else {
-      $(this).removeClass("past");
-      $(this).removeClass("present");
-      $(this).addClass("future");
-    }
-  });
-}
+          if (timeBlock < currentHour) {
+              $(this).addClass("past");
+              $(this).removeClass("future");
+              $(this).removeClass("present");
+          }
+          else if (timeBlock === currentHour) {
+              $(this).addClass("present");
+              $(this).removeClass("past");
+              $(this).removeClass("future");
+          }
+          else {
+              $(this).addClass("future");
+              $(this).removeClass("present");
+              $(this).removeClass("past");
+          }
+      })
+  }
+  hourTracker();
 
-time();
+  $(".saveBtn").on("click", function () {
+    console.log(this);
+    var text = $(this).siblings(".description").val();
+    var timeStamp = $(this).parent().attr("id");
 
-
-
-
-
-$(".saveBtn") () {
-savebtn.addEventListener('click'), function () { 
-localStorage.setItem("saved") 
-}
-
+    
+    localStorage.setItem(timeStamp, text);
+})
+})
